d7fdb3c0214283228186bdc63d7af2a1f5673748
Ronald Rajski <ronaldrajski@gmail.com>
2021-04-18 22:57:38 -0500

    dropped in Entity and generated value for id

--- src/main/java/Model/Author.java
+++ src/main/java/Model/Author.java
@@ -1,8 +1,20 @@
 package Model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Set;
 
+
+@Entity
 public class Author {
+
+    @Id
+//    Need to tell hibernate how to generate
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Long id;
     private String firstName;
     private String lastName;
     private Set<Book> books;
@@ -14,6 +26,14 @@ public class Author {
         this.firstName = firstName;
         this.lastName = lastName;
         this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFirstName() {
