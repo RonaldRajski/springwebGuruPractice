c70772ce6a3f0ec18d3b8691a7f732965b05a376
Ronald Rajski <ronaldrajski@gmail.com>
2021-04-18 23:33:56 -0500

    updated with hashcode  and toString

--- src/main/java/Model/Author.java
+++ src/main/java/Model/Author.java
@@ -1,9 +1,7 @@
 package Model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 
 
@@ -13,10 +11,12 @@ public class Author {
     @Id
 //    Need to tell hibernate how to generate
     @GeneratedValue(strategy = GenerationType.AUTO)
-
     private Long id;
+
     private String firstName;
     private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
 
     public Author(){
@@ -58,5 +58,28 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return id !=null ? id.equals(author.id) : author.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id !=null ? id.hashCode() : 0;
     }
 }
--- src/main/java/Model/Book.java
+++ src/main/java/Model/Book.java
@@ -1,9 +1,7 @@
 package Model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 
 
@@ -14,13 +12,16 @@ public class Book {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
-
     private Long id;
+
     private String title;
     private String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name="book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors;
 
-//    Must have no args consturctor always
+//    Must have no args constructor always
 
 
     public Book() {
@@ -62,6 +63,29 @@ public class Book {
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id !=null ? id.equals(book.id) : book.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id !=null ? id.hashCode() : 0;
     }
 }
 
